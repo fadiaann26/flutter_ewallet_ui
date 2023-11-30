8149792259baa3e0e3892bcb2411d5267c56504f
fadiaann26 <fadiaann26@gmail.com>
2023-11-30 11:19:44 +0700

    first commit

---
+++ lib/core.dart
@@ -0,0 +1,10 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_ewallet_ui/screen/home_screen.dart';
+export 'package:flutter_ewallet_ui/state_util.dart';
+export 'package:flutter_ewallet_ui/widgets/menu_box.dart';
+export 'package:flutter_ewallet_ui/widgets/transaction_detail.dart';
+export 'package:flutter_ewallet_ui/widgets/transaction_item.dart';
